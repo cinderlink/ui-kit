de7df22616ee4db63562978b0ec2d0ffa80e7a1b
Andrew Ewing <drew@phenocode.com>
2023-03-29 11:06:30 -0700

    feat(loading): improved loading indicator

--- src/lib/indicator/LoadingIndicator/LoadingIndicator.svelte
+++ src/lib/indicator/LoadingIndicator/LoadingIndicator.svelte
@@ -1,20 +1,65 @@
 <script lang="ts">
-	import type { Size } from '$lib/theme/types';
+	import type { Size } from '$lib/unocss/types';
+
 	export let size: Size = 'xl';
-
-	const sizeClasses = {
-		xs: 'text-xs p-1 gap-2',
-		sm: 'text-sm p-2 gap-2',
-		md: 'text-base px-4 py-2 gap-2',
-		lg: 'text-lg px-6 py-3 gap-3',
-		xl: 'text-xl px-8 py-4 gap-4',
-		'2xl': 'text-2xl p-8 gap-6'
-	}[size];
-
-	export let duration: `duration-${string}` = 'duration-1s';
+	export let icon = 'i-tabler-loader';
 </script>
 
-<div class="{sizeClasses} inline-flex items-center justify-center">
-	<div class="i-tabler-loader animate-spin {duration} ease-in-out" />
+<div class="loading-indicator loading-indicator--{size}">
+	<div class="{icon} animate-spin loading-indicator__icon" />
 	<slot />
 </div>
+
+<style>
+	.loading-indicator {
+		@apply flex flex-row items-center justify-center;
+	}
+	.loading-indicator__icon {
+		@apply text-purple-800 dark-(text-purple-300);
+	}
+	.loading-indicator--xs {
+		@apply text-xs p-1 gap-2;
+	}
+	.loading-indicator--xs .loading-indicator__icon {
+		@apply text-md;
+	}
+	.loading-indicator--sm {
+		@apply text-sm p-2 gap-2;
+	}
+	.loading-indicator--sm .loading-indicator__icon {
+		@apply text-lg;
+	}
+	.loading-indicator--md {
+		@apply text-base px-4 py-2 gap-2;
+	}
+	.loading-indicator--md .loading-indicator__icon {
+		@apply text-xl;
+	}
+	.loading-indicator--lg {
+		@apply text-lg px-6 py-3 gap-3;
+	}
+	.loading-indicator--lg .loading-indicator__icon {
+		@apply text-2xl;
+	}
+	.loading-indicator--xl {
+		@apply text-xl px-8 py-4 gap-4;
+	}
+	.loading-indicator--xl .loading-indicator__icon {
+		@apply text-3xl;
+	}
+	.loading-indicator--2xl {
+		@apply text-2xl p-8 gap-6;
+	}
+	.loading-indicator--2xl .loading-indicator__icon {
+		@apply text-4xl;
+	}
+	.loading-indicator :global(h1),
+	.loading-indicator :global(h2),
+	.loading-indicator :global(h3),
+	.loading-indicator :global(h4),
+	.loading-indicator :global(h5),
+	.loading-indicator :global(h6) {
+		@apply text-purple-800 dark-(text-purple-300);
+		@apply m-0;
+	}
+</style>
