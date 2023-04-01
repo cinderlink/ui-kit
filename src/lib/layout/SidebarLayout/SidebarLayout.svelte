cfa10350f3f9e6574717f6643ccce5231cd9121a
Andrew Ewing <drew@phenocode.com>
2023-03-31 18:13:52 -0700

    feat(mobile): first pass mobile functionality

--- src/lib/layout/SidebarLayout/SidebarLayout.svelte
+++ src/lib/layout/SidebarLayout/SidebarLayout.svelte
@@ -1,27 +1,30 @@
 <script>
-	import Panel from '$lib/content/Panel/Panel.svelte';
 	import Toggle from '$lib/interactive/Toggle/Toggle.svelte';
 	import theme from '$lib/theme/store';
+	import { sidebar } from './store';
 	import './SidebarLayout.css';
+	import { afterNavigate } from '$app/navigation';
+
+	afterNavigate(() => {
+		$sidebar.toggled = false;
+	});
 </script>
 
-<Panel classes="layout__panel">
-	<div class="layout layout--sidebar">
-		<aside class="layout__sidebar">
-			<slot name="sidebar" />
-			<div class="align-self-end">
-				<Toggle id="theme-toggle" bind:toggled={$theme.darkMode}>
-					<div slot="off" class="px-4">
-						<div class="i-tabler-sun text-yellow-200" />
-					</div>
-					<div slot="on" class="px-4">
-						<div class="i-tabler-moon text-purple-50" />
-					</div>
-				</Toggle>
-			</div>
-		</aside>
-		<div class="layout__page">
-			<slot />
+<div class="layout layout--sidebar">
+	<aside class="layout__sidebar" class:layout__sidebar--toggled={$sidebar.toggled}>
+		<slot name="sidebar" />
+		<div class="align-self-end">
+			<Toggle id="theme-toggle" bind:toggled={$theme.darkMode}>
+				<div slot="off" class="px-4">
+					<div class="i-tabler-sun text-yellow-200" />
+				</div>
+				<div slot="on" class="px-4">
+					<div class="i-tabler-moon text-purple-50" />
+				</div>
+			</Toggle>
 		</div>
+	</aside>
+	<div class="layout__page">
+		<slot />
 	</div>
-</Panel>
+</div>
--- src/lib/layout/SidebarLayout/index.ts
+++ src/lib/layout/SidebarLayout/index.ts
@@ -1,1 +1,2 @@
 export { default as SidebarLayout } from './SidebarLayout.svelte';
+export { sidebar } from './store';
